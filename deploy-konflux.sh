39c0c842191a666a9045781ea062f4d6741229f2
gbenhaim <gbenhaim@redhat.com>
2024-04-17 14:49:18 +0300

    Retry the creation of Konflux resources with CRDs
    There is a race where the kustomize tries to create the CR before
    the api digested the CRD so it fails.
    
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- deploy-konflux.sh
+++ deploy-konflux.sh
@@ -22,18 +22,33 @@ deploy() {
 
     kubectl apply -k "${script_path}/konflux-ci/rbac"
 
-    kubectl apply -k "${script_path}/konflux-ci/enterprise-contract/core"
+    retry kubectl apply -k "${script_path}/konflux-ci/enterprise-contract/core"
 
-    kubectl apply -k "${script_path}/konflux-ci/release"
+    retry kubectl apply -k "${script_path}/konflux-ci/release"
 
     # The build-service depends on CRDs from the release-service
-    kubectl apply -k "${script_path}/konflux-ci/build-service"
+    retry kubectl apply -k "${script_path}/konflux-ci/build-service"
 
     # The integration-service depends on CRDs from the release-service
-    kubectl apply -k "${script_path}/konflux-ci/integration"
+    retry kubectl apply -k "${script_path}/konflux-ci/integration"
 
     kubectl apply -k "${script_path}/konflux-ci/ui"
 }
+
+
+retry() {
+    for _ in {1..3}; do
+        local ret=0
+        "$@" || ret="$?"
+        if [[ "$ret" -eq 0 ]]; then
+            return 0
+        fi
+        sleep 3
+    done
+
+    return "$ret"
+}
+
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     main "$@"
