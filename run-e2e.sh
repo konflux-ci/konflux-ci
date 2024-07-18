548013cc20d9a7429e041914b2f89fe5cb50621e
Pavel Sturc <psturc@redhat.com>
2024-07-19 00:28:01 +0200

    debug: print PLRs, TRs, logs

--- run-e2e.sh
+++ run-e2e.sh
@@ -7,7 +7,13 @@ main() {
     local github_org="${GH_ORG:?A GitHub org where the https://github.com/redhat-appstudio-qe/konflux-ci-sample repo is present/forked should be provided}"
     local github_token="${GH_TOKEN:?A GitHub token should be provided}"
     local quay_dockerconfig="${QUAY_DOCKERCONFIGJSON:?quay.io credentials in .dockerconfig format should be provided}"
-    docker run --network=host -v ~/.kube/config:/kube/config --env KUBECONFIG=/kube/config -e GITHUB_TOKEN="$github_token" -e QUAY_TOKEN="$(base64 <<< "$quay_dockerconfig")" -e MY_GITHUB_ORG="$github_org" -e E2E_APPLICATIONS_NAMESPACE=user-ns2 $E2E_TEST_IMAGE "--ginkgo.label-filter=upstream-konflux" "--ginkgo.focus=Test local" "--ginkgo.v"
+    local ret=0
+    docker run --network=host -v ~/.kube/config:/kube/config --env KUBECONFIG=/kube/config -e GITHUB_TOKEN="$github_token" -e QUAY_TOKEN="$(base64 <<< "$quay_dockerconfig")" -e MY_GITHUB_ORG="$github_org" -e E2E_APPLICATIONS_NAMESPACE=user-ns2 $E2E_TEST_IMAGE "--ginkgo.label-filter=upstream-konflux" "--ginkgo.focus=Test local" "--ginkgo.v" || ret="$?"
+    if [ "$ret" != "0" ]; then
+        kubectl get pr -A -o yaml
+        kubectl get tr -A -o yaml
+        for pod in $(kubectl get pods -n user-ns2 -o name); do echo "Logs for $pod:" && kubectl logs "$pod" -n user-ns2 --all-containers=true; done
+    fi
 }
 
 
