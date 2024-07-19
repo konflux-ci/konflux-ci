3434b74929ad8fe7c21d99ad016b42fef3c777f9
Pavel Sturc <psturc@redhat.com>
2024-07-19 14:15:55 +0200

    more logs

--- run-e2e.sh
+++ run-e2e.sh
@@ -11,6 +11,7 @@ main() {
     docker run --network=host -v ~/.kube/config:/kube/config --env KUBECONFIG=/kube/config -e GITHUB_TOKEN="$github_token" -e QUAY_TOKEN="$(base64 <<< "$quay_dockerconfig")" -e MY_GITHUB_ORG="$github_org" -e E2E_APPLICATIONS_NAMESPACE=user-ns2 $E2E_TEST_IMAGE "--ginkgo.label-filter=upstream-konflux" "--ginkgo.focus=Test local" "--ginkgo.v" || ret="$?"
     if [ "$ret" != "0" ]; then
         set -x
+        kubectl get nodes -o yaml
         kubectl run clair2 -n default --attach=true --restart=Never --image quay.io/redhat-appstudio/clair-in-ci:v1 --command -- "clair-action" "report" "--image-ref=quay.io/psturc_org/user-ns2/konflux-ci-upstream/konflux-ci-upstream@sha256:06ed74b1a0e4cce488ef5f831fcd403385bda21ad2ec27d034b8c1a54ed59fb2" "--db-path=/tmp/matcher.db" "--format=quay"
         kubectl get pr -A -o yaml
         kubectl get tr -A -o yaml
