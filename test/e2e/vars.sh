a341e6dcde709fe7ac2699cf2434135ae6953e40
Pavel Sturc <psturc@redhat.com>
2024-12-12 13:02:36 +0100

    fix: update e2e-tests image and cmd

--- test/e2e/vars.sh
+++ test/e2e/vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 export E2E_TEST_IMAGE CUSTOMIZED_DOCKER_PIPELINE_IMAGE_REF_LOCALHOST CUSTOMIZED_DOCKER_PIPELINE_IMAGE_REF_CLUSTER CUSTOMIZED_BUILD_TASK_IMAGE_REF_LOCALHOST CUSTOMIZED_BUILD_TASK_IMAGE_REF_CLUSTER
 # https://github.com/konflux-ci/e2e-tests/commit/<COMMIT>
-E2E_TEST_IMAGE=quay.io/konflux-ci/e2e-tests:f6e28a7aeab5a227d1a7b31079f27a58ad75ef58
+E2E_TEST_IMAGE=quay.io/redhat-user-workloads/konflux-qe-team-tenant/konflux-e2e/konflux-e2e-tests@sha256:c3881d28a91c0d406506ed807fcf461bc2ad830622bed20f4f5602e981fb4718
 CUSTOMIZED_DOCKER_PIPELINE_IMAGE_REF_LOCALHOST="localhost:30001/test/test:customized-docker-pipeline"
 CUSTOMIZED_BUILD_TASK_IMAGE_REF_LOCALHOST="localhost:30001/test/test:customized-build-task"
 CUSTOMIZED_DOCKER_PIPELINE_IMAGE_REF_CLUSTER="registry-service.kind-registry/test/test:customized-docker-pipeline"
