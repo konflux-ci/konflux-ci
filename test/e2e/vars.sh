0a654984c109992b20f915390a17dbddd636ff91
Pavel Sturc <psturc@redhat.com>
2025-03-25 16:42:30 +0100

    REVERT: use e2e image from a PR

--- test/e2e/vars.sh
+++ test/e2e/vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 export E2E_TEST_IMAGE
 # https://github.com/konflux-ci/e2e-tests/commit/<COMMIT>
-E2E_TEST_IMAGE=quay.io/redhat-user-workloads/konflux-qe-team-tenant/konflux-e2e/konflux-e2e-tests@sha256:2a0912769f1d040274885324d2a7d626cb8ca4cb2527008d8cd424326cf59dff
+E2E_TEST_IMAGE=quay.io/redhat-user-workloads/konflux-qe-team-tenant/konflux-e2e/konflux-e2e-tests:on-pr-82b3541aa51878c94658c7d096a3a11d3f4bc5ae
