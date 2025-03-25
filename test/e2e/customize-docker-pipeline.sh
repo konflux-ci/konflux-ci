3cd7a3cd6ea4e6ab2420b7199ec55484ef58aae1
Pavel Sturc <psturc@redhat.com>
2025-03-25 16:37:22 +0100

    test: modify and test docker-build-oci-ta pipeline

--- test/e2e/customize-docker-pipeline.sh
+++ test/e2e/customize-docker-pipeline.sh
@@ -1,7 +1,43 @@
-#!/bin/bash -e
-set -x
+#!/bin/bash
+set -euo pipefail
 
 script_path="$(dirname -- "${BASH_SOURCE[0]}")"
+
+update_pipeline() {
+    local pipeline_name=$1
+    local tmp_pipeline_path="/tmp/customized-${pipeline_name}.yaml"
+    local original_pipeline_bundle_ref
+
+    original_pipeline_bundle_ref=$(yq ".data[\"config.yaml\"]" konflux-ci/build-service/core/build-pipeline-config.yaml | yq ".pipelines[] | select(.name == \"${pipeline_name}\").bundle")
+    tkn bundle list --remote-skip-tls "$original_pipeline_bundle_ref" -o yaml > "${tmp_pipeline_path}"
+
+    for task_name in $(yq "(.spec.tasks[] | .name)" "${tmp_pipeline_path}");
+    do
+        local original_task_bundle_ref
+        local tmp_task_path="/tmp/customized-task.yaml"
+        local customized_task_image_ref_localhost="localhost:30001/test/test:customized-task-${task_name}-${pipeline_name}"
+        local customized_task_image_ref_cluster="registry-service.kind-registry/test/test:customized-task-${task_name}-${pipeline_name}"
+
+        original_task_bundle_ref=$(yq "(.spec.tasks[] | select(.name == \"${task_name}\").taskRef.params[] | select(.name == \"bundle\").value)" "${tmp_pipeline_path}")
+        tkn bundle list --remote-skip-tls "$original_task_bundle_ref" -o yaml > $tmp_task_path || continue
+
+        yq e -i '(.spec.steps[] | select(has("computeResources")) | .computeResources.requests.cpu) |= "100m"' $tmp_task_path
+        yq e -i '(.spec.steps[] | select(has("computeResources")) | .computeResources.requests.memory) |= "256Mi"' $tmp_task_path
+        yq e -i '(.spec.steps[] | select(has("resources")) | .resources.requests.cpu) |= "100m"' $tmp_task_path
+        yq e -i '(.spec.steps[] | select(has("resources")) | .resources.requests.memory) |= "256Mi"' $tmp_task_path
+
+        yq e -i "(.spec.tasks[] | select(.name == \"${task_name}\").taskRef.params[] | select(.name == \"bundle\").value) |= \"${customized_task_image_ref_cluster}\"" "${tmp_pipeline_path}"
+
+        tkn bundle push --remote-skip-tls "$customized_task_image_ref_localhost" -f $tmp_task_path
+    done
+
+    local customized_pipeline_image_ref_localhost="localhost:30001/test/test:customized-${pipeline_name}-pipeline"
+    tkn bundle push --remote-skip-tls "$customized_pipeline_image_ref_localhost" -f "${tmp_pipeline_path}"
+
+    # Update the bundle ref in build-service pipeline configmap
+    local customized_pipeline_image_ref_cluster="registry-service.kind-registry/test/test:customized-${pipeline_name}-pipeline"
+    sed -i "s|bundle:.*${pipeline_name}@.*|bundle: ${customized_pipeline_image_ref_cluster}|g" konflux-ci/build-service/core/build-pipeline-config.yaml
+}
 
 main() {
     echo "Updating docker-build pipeline to workaround the issues with and cpu/mem requests for build task" >&2
@@ -11,28 +47,7 @@ main() {
     # This is required in order to push the modified pipeline to local registry
     kubectl port-forward -n kind-registry svc/registry-service 30001:443 &
 
-    local original_docker_build_bundle_ref
-    local tmp_pipeline_path="/tmp/customized-docker-pipeline.yaml"
-    local tmp_build_task_path="/tmp/customized-build-container-task.yaml"
-    # Pull the required Pipeline and Task bundles
-    original_docker_build_bundle_ref=$(yq ".data[\"config.yaml\"]" konflux-ci/build-service/core/build-pipeline-config.yaml | yq ".pipelines[] | select(.name == \"docker-build\").bundle")
-    tkn bundle list --remote-skip-tls "$original_docker_build_bundle_ref" -o yaml > $tmp_pipeline_path
-    original_build_task_bundle_ref=$(yq "(.spec.tasks[] | select(.name == \"build-container\").taskRef.params[] | select(.name == \"bundle\").value)" $tmp_pipeline_path)
-    tkn bundle list --remote-skip-tls "$original_build_task_bundle_ref" -o yaml > $tmp_build_task_path
-    # Workaround
-    # CPU and memory requests are too high for the build-container Task's "build" step to run in GitHub actions
-    # Thus this workaround lowers ".computeResources.requests" from the "build" step and updates the build Pipeline that is used in CI to reference the updated Task
-    yq e -i "(.spec.steps[].computeResources.requests.cpu) |= \"100m\"" $tmp_build_task_path
-    yq e -i "(.spec.steps[].computeResources.requests.memory) |= \"256Mi\"" $tmp_build_task_path
-    yq e -i "(.spec.tasks[] | select(.name == \"build-container\").taskRef.params[] | select(.name == \"bundle\").value) |= \"${CUSTOMIZED_BUILD_TASK_IMAGE_REF_CLUSTER}\"" $tmp_pipeline_path
-
-    # Push the modified Pipeline and Task bundles to the local container registry
-    tkn bundle push --remote-skip-tls "$CUSTOMIZED_DOCKER_PIPELINE_IMAGE_REF_LOCALHOST" \
-        -f $tmp_pipeline_path
-    tkn bundle push --remote-skip-tls "$CUSTOMIZED_BUILD_TASK_IMAGE_REF_LOCALHOST" \
-        -f $tmp_build_task_path
-    # Update the bundle ref in build-service pipeline configmap
-    sed -i "s|bundle:.*docker-build.*|bundle: ${CUSTOMIZED_DOCKER_PIPELINE_IMAGE_REF_CLUSTER}|g" konflux-ci/build-service/core/build-pipeline-config.yaml
+    update_pipeline docker-build-oci-ta
 }
 
 
