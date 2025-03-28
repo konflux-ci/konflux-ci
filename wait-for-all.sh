8dfde0ece0bbdc1cac23896cb8bc2f091717ed0b
Francesco Ilario <filario@redhat.com>
2025-03-28 15:28:37 +0100

    remove workspaces manager in favor of konflux-ci/namespace-lister
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- wait-for-all.sh
+++ wait-for-all.sh
@@ -2,7 +2,7 @@
 
 main() {
     kubectl wait --for=condition=Ready tektonconfig/config --timeout=120s
-    kubectl wait --for=condition=Available deployment --all -A --timeout=180s
+    kubectl wait --for=condition=Available deployment --all -A --timeout=240s
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
