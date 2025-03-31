218d6017ef03d07067af6306448ba7d66dcd0e30
Pavel Sturc <psturc@redhat.com>
2025-03-31 13:26:26 +0200

    fix: use updated e2e image

--- test/e2e/vars.sh
+++ test/e2e/vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 export E2E_TEST_IMAGE
 # https://github.com/konflux-ci/e2e-tests/commit/<COMMIT>
-E2E_TEST_IMAGE=quay.io/redhat-user-workloads/konflux-qe-team-tenant/konflux-e2e/konflux-e2e-tests@sha256:2a0912769f1d040274885324d2a7d626cb8ca4cb2527008d8cd424326cf59dff
+E2E_TEST_IMAGE=quay.io/redhat-user-workloads/konflux-qe-team-tenant/konflux-e2e/konflux-e2e-tests@sha256:e9a739d65c5f7809c29103217d8bc36a419cf3ff41be04c89f10324c4be9fd21
