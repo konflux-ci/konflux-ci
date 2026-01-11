konflux-ci/konflux-ci

commit 4d82bfc37363375335e3c741e7f4604633d9cbd0

Move status handling code to a shared file
Reduce duplication by making the code responsible for managing
the status of CRs generic and place it in a dedicated file.

Signed-off-by: Gal Ben Haim <gbenhaim@redhat.com>

operator/internal/controller/conditions.go
@@ -1,0 +1,263 @@
+/*
+Copyright 2025 Konflux CI.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"fmt"
+	"strings"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	konfluxv1alpha1 "github.com/konflux-ci/konflux-ci/operator/api/v1alpha1"
+)
+
+// SetCondition updates or adds a condition to a resource's status.
+// It automatically sets LastTransitionTime and ObservedGeneration.
+// If the condition status hasn't changed, LastTransitionTime is preserved.
+func SetCondition(obj konfluxv1alpha1.ConditionAccessor, condition metav1.Condition) {
+	condition.ObservedGeneration = obj.GetGeneration()
+
+	conditions := obj.GetConditions()
+	apimeta.SetStatusCondition(&conditions, condition)
+	obj.SetConditions(conditions)
+}
+
+// SetFailedCondition sets a failed condition with ConditionFalse status on the resource.
+// This is a convenience function for error handling in reconcilers.
+// The caller is responsible for persisting the status update.
+func SetFailedCondition(obj konfluxv1alpha1.ConditionAccessor, conditionType string, reason string, err error) {
+	SetCondition(obj, metav1.Condition{
+		Type:    conditionType,
+		Status:  metav1.ConditionFalse,
+		Reason:  reason,
+		Message: err.Error(),
+	})
+}
+
+// CleanupStaleConditions removes conditions that don't match the shouldKeep predicate.
+// This is useful for removing conditions for resources that no longer exist.
+func CleanupStaleConditions(obj konfluxv1alpha1.ConditionAccessor, shouldKeep func(cond metav1.Condition) bool) {
+	var newConditions []metav1.Condition
+	for _, cond := range obj.GetConditions() {
+		if shouldKeep(cond) {
+			newConditions = append(newConditions, cond)
+		}
+	}
+	obj.SetConditions(newConditions)
+}
+
+// IsDeploymentReady returns true if the deployment has all replicas ready and updated.
+func IsDeploymentReady(deployment *appsv1.Deployment) bool {
+	return deployment.Status.ReadyReplicas == deployment.Status.Replicas &&
+		deployment.Status.Replicas > 0 &&
+		deployment.Status.UpdatedReplicas == deployment.Status.Replicas
+}
+
+// DeploymentCondition creates a condition representing the current state of a deployment.
+// The condition type is formatted as "namespace/name".
+func DeploymentCondition(deployment *appsv1.Deployment) metav1.Condition {
+	conditionType := fmt.Sprintf("%s/%s", deployment.Namespace, deployment.Name)
+
+	if IsDeploymentReady(deployment) {
+		return metav1.Condition{
+			Type:    conditionType,
+			Status:  metav1.ConditionTrue,
+			Reason:  "DeploymentReady",
+			Message: fmt.Sprintf("Deployment has %d/%d replicas ready", deployment.Status.ReadyReplicas, deployment.Status.Replicas),
+		}
+	}
+
+	message := fmt.Sprintf("Ready: %d/%d, Updated: %d/%d",
+		deployment.Status.ReadyReplicas, deployment.Status.Replicas,
+		deployment.Status.UpdatedReplicas, deployment.Status.Replicas)
+
+	// Check for specific conditions that indicate problems
+	for _, cond := range deployment.Status.Conditions {
+		if cond.Type == appsv1.DeploymentProgressing && cond.Status == corev1.ConditionFalse {
+			message = fmt.Sprintf("%s - %s: %s", message, cond.Reason, cond.Message)
+		}
+		if cond.Type == appsv1.DeploymentReplicaFailure && cond.Status == corev1.ConditionTrue {
+			message = fmt.Sprintf("%s - ReplicaFailure: %s", message, cond.Message)
+		}
+	}
+
+	return metav1.Condition{
+		Type:    conditionType,
+		Status:  metav1.ConditionFalse,
+		Reason:  "DeploymentNotReady",
+		Message: message,
+	}
+}
+
+// DeploymentStatusSummary contains aggregated status information about a set of deployments.
+type DeploymentStatusSummary struct {
+	// AllReady is true if all deployments are ready.
+	AllReady bool
+	// TotalCount is the total number of deployments.
+	TotalCount int
+	// NotReadyNames contains the names of deployments that are not ready.
+	NotReadyNames []string
+	// SeenConditionTypes contains the condition types for all processed deployments.
+	SeenConditionTypes map[string]bool
+}
+
+// SetDeploymentConditions sets conditions on the object for each deployment in the list
+// and returns a summary of the deployment statuses.
+func SetDeploymentConditions(obj konfluxv1alpha1.ConditionAccessor, deployments []appsv1.Deployment) DeploymentStatusSummary {
+	summary := DeploymentStatusSummary{
+		AllReady:           true,
+		TotalCount:         len(deployments),
+		SeenConditionTypes: make(map[string]bool),
+	}
+
+	for i := range deployments {
+		deployment := &deployments[i]
+		cond := DeploymentCondition(deployment)
+		summary.SeenConditionTypes[cond.Type] = true
+
+		if !IsDeploymentReady(deployment) {
+			summary.AllReady = false
+			summary.NotReadyNames = append(summary.NotReadyNames, deployment.Name)
+		}
+
+		SetCondition(obj, cond)
+	}
+
+	return summary
+}
+
+// SetOverallReadyCondition sets the overall Ready condition based on the deployment status summary.
+func SetOverallReadyCondition(obj konfluxv1alpha1.ConditionAccessor, readyConditionType string, summary DeploymentStatusSummary) {
+	if summary.AllReady {
+		SetCondition(obj, metav1.Condition{
+			Type:    readyConditionType,
+			Status:  metav1.ConditionTrue,
+			Reason:  "AllComponentsReady",
+			Message: fmt.Sprintf("All %d deployments are ready", summary.TotalCount),
+		})
+	} else {
+		SetCondition(obj, metav1.Condition{
+			Type:    readyConditionType,
+			Status:  metav1.ConditionFalse,
+			Reason:  "ComponentsNotReady",
+			Message: fmt.Sprintf("Deployments not ready: %v", summary.NotReadyNames),
+		})
+	}
+}
+
+// IsConditionTrue returns true if the specified condition type has status True.
+func IsConditionTrue(obj konfluxv1alpha1.ConditionAccessor, conditionType string) bool {
+	cond := apimeta.FindStatusCondition(obj.GetConditions(), conditionType)
+	return cond != nil && cond.Status == metav1.ConditionTrue
+}
+
+// SubCRStatus represents the readiness status of a sub-CR.
+type SubCRStatus struct {
+	// Name is the name/identifier of the sub-CR (e.g., "KonfluxBuildService")
+	Name string
+	// Ready indicates if the sub-CR is ready
+	Ready bool
+}
+
+// AggregateReadiness computes the overall readiness from deployment summary and sub-CR statuses.
+// Returns whether all components are ready and a slice of reasons for any not-ready components.
+func AggregateReadiness(deploymentSummary DeploymentStatusSummary, subCRStatuses []SubCRStatus) (allReady bool, notReadyReasons []string) {
+	allReady = deploymentSummary.AllReady
+
+	if !deploymentSummary.AllReady {
+		notReadyReasons = append(notReadyReasons, fmt.Sprintf("deployments not ready: %v", deploymentSummary.NotReadyNames))
+	}
+
+	for _, status := range subCRStatuses {
+		if !status.Ready {
+			allReady = false
+			notReadyReasons = append(notReadyReasons, fmt.Sprintf("%s is not ready", status.Name))
+		}
+	}
+
+	return allReady, notReadyReasons
+}
+
+// SetAggregatedReadyCondition sets the overall Ready condition based on aggregated component readiness.
+func SetAggregatedReadyCondition(
+	obj konfluxv1alpha1.ConditionAccessor,
+	readyConditionType string,
+	deploymentSummary DeploymentStatusSummary,
+	subCRStatuses []SubCRStatus,
+) {
+	allReady, notReadyReasons := AggregateReadiness(deploymentSummary, subCRStatuses)
+
+	if allReady {
+		// Count total components (deployments + sub-CRs)
+		totalComponents := deploymentSummary.TotalCount + len(subCRStatuses)
+		SetCondition(obj, metav1.Condition{
+			Type:    readyConditionType,
+			Status:  metav1.ConditionTrue,
+			Reason:  "AllComponentsReady",
+			Message: fmt.Sprintf("All %d components are ready", totalComponents),
+		})
+	} else {
+		SetCondition(obj, metav1.Condition{
+			Type:    readyConditionType,
+			Status:  metav1.ConditionFalse,
+			Reason:  "ComponentsNotReady",
+			Message: strings.Join(notReadyReasons, "; "),
+		})
+	}
+}
+
+// CopySubCRStatus copies conditions from a sub-CR to a parent CR with a prefix.
+// It removes existing conditions with the prefix, copies new ones, and returns the sub-CR's ready status.
+// The caller is responsible for fetching the sub-CR before calling this function.
+func CopySubCRStatus(
+	parent konfluxv1alpha1.ConditionAccessor,
+	subCR konfluxv1alpha1.ConditionAccessor,
+	conditionPrefix string,
+) SubCRStatus {
+	// Remove existing conditions with this prefix
+	prefixWithDot := conditionPrefix + "."
+	var newConditions []metav1.Condition
+	for _, cond := range parent.GetConditions() {
+		if !strings.HasPrefix(cond.Type, prefixWithDot) {
+			newConditions = append(newConditions, cond)
+		}
+	}
+	parent.SetConditions(newConditions)
+
+	// Copy conditions from sub-CR to parent, prefixing with the condition prefix
+	for _, cond := range subCR.GetConditions() {
+		// Replace slashes with dots in the condition type to avoid multiple slashes
+		sanitizedType := strings.ReplaceAll(cond.Type, "/", ".")
+		// Prefix the condition type to namespace it under the sub-CR
+		prefixedType := fmt.Sprintf("%s.%s", conditionPrefix, sanitizedType)
+		SetCondition(parent, metav1.Condition{
+			Type:               prefixedType,
+			Status:             cond.Status,
+			Reason:             cond.Reason,
+			Message:            cond.Message,
+			ObservedGeneration: cond.ObservedGeneration,
+		})
+	}
+
+	return SubCRStatus{
+		Name:  conditionPrefix,
+		Ready: IsConditionTrue(subCR, "Ready"),
+	}
+}

operator/api/v1alpha1/condition_accessor.go
@@ -1,0 +1,31 @@
+/*
+Copyright 2025 Konflux CI.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ConditionAccessor is implemented by types that have a Conditions slice in their Status.
+// This interface allows for generic condition management across different CR types.
+// +kubebuilder:object:generate=false
+type ConditionAccessor interface {
+	client.Object
+	GetConditions() []metav1.Condition
+	SetConditions(conditions []metav1.Condition)
+}

operator/api/v1alpha1/konflux_types.go
@@ -74,6 +74,16 @@ type KonfluxList struct {
 	Items           []Konflux `json:"items"`
 }
 
+// GetConditions returns the conditions from the Konflux status.
+func (k *Konflux) GetConditions() []metav1.Condition {
+	return k.Status.Conditions
+}
+
+// SetConditions sets the conditions on the Konflux status.
+func (k *Konflux) SetConditions(conditions []metav1.Condition) {
+	k.Status.Conditions = conditions
+}
+
 func init() {
 	SchemeBuilder.Register(&Konflux{}, &KonfluxList{})
 }

operator/api/v1alpha1/konfluxbuildservice_types.go
@@ -62,6 +62,16 @@ type KonfluxBuildServiceList struct {
 	Items           []KonfluxBuildService `json:"items"`
 }
 
+// GetConditions returns the conditions from the KonfluxBuildService status.
+func (k *KonfluxBuildService) GetConditions() []metav1.Condition {
+	return k.Status.Conditions
+}
+
+// SetConditions sets the conditions on the KonfluxBuildService status.
+func (k *KonfluxBuildService) SetConditions(conditions []metav1.Condition) {
+	k.Status.Conditions = conditions
+}
+
 func init() {
 	SchemeBuilder.Register(&KonfluxBuildService{}, &KonfluxBuildServiceList{})
 }

commit a08f0a202a3105a7a3cb9d97844c1933cb7ef40a

fix: prevent infinite reconcile loops in CopySubCRStatus
CopySubCRStatus was deleting all prefixed conditions before re-adding
them, causing SetStatusCondition to treat them as new and set
LastTransitionTime to the current time on every reconcile.

The bug was introduced in 4d82bfc3 but only manifested intermittently:
when reconciles completed within the same second, metav1.Time's
second-level precision meant LastTransitionTime appeared unchanged,
preventing the loop. When reconciles crossed second boundaries, the
timestamps differed, triggering status updates and watch events in an
infinite loop.

The bug became more visible after adding the tracking client, which
introduced CleanupOrphans API calls that slowed reconciliation enough
to consistently cross second boundaries.

The fix reorders operations: update conditions first (letting
SetStatusCondition preserve LastTransitionTime when status is unchanged),
then remove stale conditions afterward.

Assisted-By: Cursor
Signed-off-by: Gal Ben Haim <gbenhaim@redhat.com>

operator/internal/controller/conditions.go
@@ -235,22 +235,21 @@ func CopySubCRStatus(
 	subCR konfluxv1alpha1.ConditionAccessor,
 	conditionPrefix string,
 ) SubCRStatus {
-	// Remove existing conditions with this prefix
 	prefixWithDot := conditionPrefix + "."
-	var newConditions []metav1.Condition
-	for _, cond := range parent.GetConditions() {
-		if !strings.HasPrefix(cond.Type, prefixWithDot) {
-			newConditions = append(newConditions, cond)
-		}
-	}
-	parent.SetConditions(newConditions)
 
-	// Copy conditions from sub-CR to parent, prefixing with the condition prefix
+	// Track which prefixed conditions we're updating (to detect stale ones)
+	updatedConditions := make(map[string]bool)
+
+	// Copy conditions from sub-CR to parent, prefixing with the condition prefix.
+	// SetCondition (via apimeta.SetStatusCondition) handles LastTransitionTime correctly:
+	// it only updates the time when the status actually changes.
 	for _, cond := range subCR.GetConditions() {
 		// Replace slashes with dots in the condition type to avoid multiple slashes
 		sanitizedType := strings.ReplaceAll(cond.Type, "/", ".")
 		// Prefix the condition type to namespace it under the sub-CR
 		prefixedType := fmt.Sprintf("%s.%s", conditionPrefix, sanitizedType)
+		updatedConditions[prefixedType] = true
+
 		SetCondition(parent, metav1.Condition{
 			Type:               prefixedType,
 			Status:             cond.Status,
@@ -259,6 +258,17 @@ func CopySubCRStatus(
 			ObservedGeneration: cond.ObservedGeneration,
 		})
 	}
+
+	// Remove stale conditions (prefixed conditions that no longer exist in sub-CR)
+	parentConditions := parent.GetConditions()
+	newConditions := make([]metav1.Condition, 0, len(parentConditions))
+	for _, cond := range parentConditions {
+		if strings.HasPrefix(cond.Type, prefixWithDot) && !updatedConditions[cond.Type] {
+			continue // Skip stale condition
+		}
+		newConditions = append(newConditions, cond)
+	}
+	parent.SetConditions(newConditions)
 
 	return SubCRStatus{
 		Name:  conditionPrefix,

operator/pkg/tracking/tracking.go
@@ -61,6 +61,7 @@ import (
 	"errors"
 	"fmt"
 	"sync"
+	"time"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -314,6 +315,7 @@ func (c *Client) CleanupOrphans(
 	gvks []schema.GroupVersionKind,
 ) error {
 	log := logf.FromContext(ctx)
+	start := time.Now()
 
 	for _, gvk := range gvks {
 		if err := c.cleanupOrphansForGVK(ctx, ownerLabelKey, ownerLabelValue, gvk); err != nil {
@@ -322,6 +324,7 @@ func (c *Client) CleanupOrphans(
 		}
 	}
 
+	log.Info("CleanupOrphans completed", "duration", time.Since(start), "gvkCount", len(gvks))
 	return nil
 }
 

operator/internal/controller/conditions_test.go
@@ -342,4 +342,188 @@ var _ = Describe("Conditions Helper Functions", func() {
 			Expect(condition.Message).To(Equal("something went wrong"))
 		})
 	})
+
+	Describe("CopySubCRStatus", func() {
+		var parent *konfluxv1alpha1.Konflux
+		var subCR *konfluxv1alpha1.KonfluxBuildService
+		var originalTime metav1.Time
+
+		BeforeEach(func() {
+			// Set a fixed time in the past for the original condition
+			originalTime = metav1.NewTime(metav1.Now().Add(-1 * 60 * 60 * 1000000000)) // 1 hour ago
+
+			parent = &konfluxv1alpha1.Konflux{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:       "test-konflux",
+					Generation: 1,
+				},
+			}
+
+			subCR = &konfluxv1alpha1.KonfluxBuildService{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:       "test-build-service",
+					Generation: 1,
+				},
+			}
+		})
+
+		It("should preserve LastTransitionTime when status hasn't changed", func() {
+			// Simulate a previous reconcile: parent already has a condition from sub-CR
+			parent.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "build-service.Ready",
+					Status:             metav1.ConditionTrue,
+					Reason:             "AllComponentsReady",
+					Message:            "All components are ready",
+					LastTransitionTime: originalTime,
+					ObservedGeneration: 1,
+				},
+			}
+
+			// Sub-CR has the same status (True -> True, no change)
+			subCR.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "Ready",
+					Status:             metav1.ConditionTrue,
+					Reason:             "AllComponentsReady",
+					Message:            "All components are ready",
+					LastTransitionTime: metav1.Now(), // Sub-CR's own LastTransitionTime (irrelevant)
+					ObservedGeneration: 1,
+				},
+			}
+
+			// Call CopySubCRStatus - this simulates a subsequent reconcile
+			CopySubCRStatus(parent, subCR, "build-service")
+
+			// The key assertion: LastTransitionTime should NOT change because
+			// the status (ConditionTrue) hasn't changed
+			condition := apimeta.FindStatusCondition(parent.GetConditions(), "build-service.Ready")
+			Expect(condition).NotTo(BeNil())
+			Expect(condition.Status).To(Equal(metav1.ConditionTrue))
+			Expect(condition.LastTransitionTime).To(Equal(originalTime),
+				"LastTransitionTime should be preserved when status hasn't changed. "+
+					"BUG: If this fails, it means CopySubCRStatus is resetting LastTransitionTime "+
+					"on every call, which causes infinite reconcile loops.")
+		})
+
+		It("should update LastTransitionTime when status changes", func() {
+			// Parent has an existing condition with status True
+			parent.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "build-service.Ready",
+					Status:             metav1.ConditionTrue,
+					Reason:             "AllComponentsReady",
+					Message:            "All components are ready",
+					LastTransitionTime: originalTime,
+					ObservedGeneration: 1,
+				},
+			}
+
+			// Sub-CR now has status False (a real change)
+			subCR.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "Ready",
+					Status:             metav1.ConditionFalse,
+					Reason:             "ComponentNotReady",
+					Message:            "Deployment not ready",
+					LastTransitionTime: metav1.Now(),
+					ObservedGeneration: 1,
+				},
+			}
+
+			CopySubCRStatus(parent, subCR, "build-service")
+
+			condition := apimeta.FindStatusCondition(parent.GetConditions(), "build-service.Ready")
+			Expect(condition).NotTo(BeNil())
+			Expect(condition.Status).To(Equal(metav1.ConditionFalse))
+			// LastTransitionTime SHOULD be updated because status changed
+			Expect(condition.LastTransitionTime).NotTo(Equal(originalTime),
+				"LastTransitionTime should be updated when status changes from True to False")
+		})
+
+		It("should copy conditions with correct prefix", func() {
+			subCR.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "Ready",
+					Status:             metav1.ConditionTrue,
+					Reason:             "AllReady",
+					Message:            "All ready",
+					LastTransitionTime: metav1.Now(),
+				},
+				{
+					Type:               "default/deployment-1",
+					Status:             metav1.ConditionTrue,
+					Reason:             "DeploymentReady",
+					Message:            "Deployment ready",
+					LastTransitionTime: metav1.Now(),
+				},
+			}
+
+			CopySubCRStatus(parent, subCR, "build-service")
+
+			// Check prefixed conditions exist
+			readyCond := apimeta.FindStatusCondition(parent.GetConditions(), "build-service.Ready")
+			Expect(readyCond).NotTo(BeNil())
+
+			// Slashes should be replaced with dots
+			deploymentCond := apimeta.FindStatusCondition(parent.GetConditions(), "build-service.default.deployment-1")
+			Expect(deploymentCond).NotTo(BeNil())
+		})
+
+		It("should return correct ready status", func() {
+			subCR.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "Ready",
+					Status:             metav1.ConditionTrue,
+					Reason:             "AllReady",
+					LastTransitionTime: metav1.Now(),
+				},
+			}
+
+			status := CopySubCRStatus(parent, subCR, "build-service")
+
+			Expect(status.Name).To(Equal("build-service"))
+			Expect(status.Ready).To(BeTrue())
+		})
+
+		It("should remove stale conditions for sub-CR", func() {
+			// Parent has old conditions from a previous state
+			parent.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "build-service.Ready",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: originalTime,
+				},
+				{
+					Type:               "build-service.old-deployment",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: originalTime,
+				},
+				{
+					Type:               "other-component.Ready", // Different component, should be kept
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: originalTime,
+				},
+			}
+
+			// Sub-CR only has Ready condition now (old-deployment is gone)
+			subCR.Status.Conditions = []metav1.Condition{
+				{
+					Type:               "Ready",
+					Status:             metav1.ConditionTrue,
+					Reason:             "AllReady",
+					LastTransitionTime: metav1.Now(),
+				},
+			}
+
+			CopySubCRStatus(parent, subCR, "build-service")
+
+			// build-service.Ready should exist
+			Expect(apimeta.FindStatusCondition(parent.GetConditions(), "build-service.Ready")).NotTo(BeNil())
+			// build-service.old-deployment should be removed
+			Expect(apimeta.FindStatusCondition(parent.GetConditions(), "build-service.old-deployment")).To(BeNil())
+			// other-component.Ready should still exist
+			Expect(apimeta.FindStatusCondition(parent.GetConditions(), "other-component.Ready")).NotTo(BeNil())
+		})
+	})
 })