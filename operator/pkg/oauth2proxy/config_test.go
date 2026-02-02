0751f3e1ef2b3ac2b94592defc19f866a5d51496
Avi Biton <abiton@redhat.com>
2026-02-02 12:08:28 +0200

    fix(KFLUXVNGD-655): Enable proper certificate validation for Dex authentication
    - Create a new Certificate (`oauth2-proxy-cert`) to issue the CA certificate
    - Create a new Secret (`oauth2-proxy-cert`) to store the CA certificate
    - Use the same issuer as the other certificates (`ca-issuer`)
    - Add a new Volume (`ca-bundle`) to mount the CA certificate to the `oauth2-proxy` container
    - Add a new Environment variable (`OAUTH2_PROXY_PROVIDER_CA_FILES`) to point to the CA certificate
    - Add/update tests
    
    Signed-off-by: Avi Biton <abiton@redhat.com>

--- operator/pkg/oauth2proxy/config_test.go
+++ operator/pkg/oauth2proxy/config_test.go
@@ -137,13 +137,22 @@ func TestWithAuthSettings(t *testing.T) {
 	g.Expect(envMap["OAUTH2_PROXY_SKIP_JWT_BEARER_TOKENS"]).To(Equal("true"))
 }
 
-func TestWithTLSSkipVerify(t *testing.T) {
+func TestWithCABundle(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	envVars := applyOption(WithTLSSkipVerify())
-	envMap := envVarsToMap(envVars)
+	c := &corev1.Container{}
+	WithCABundle()(c, customization.DeploymentContext{})
 
-	g.Expect(envMap["OAUTH2_PROXY_SSL_INSECURE_SKIP_VERIFY"]).To(Equal("true"))
+	// Check environment variable
+	envMap := envVarsToMap(c.Env)
+	g.Expect(envMap["OAUTH2_PROXY_PROVIDER_CA_FILES"]).To(Equal(CABundleMountPath))
+
+	// Check volume mount
+	g.Expect(c.VolumeMounts).To(HaveLen(1))
+	g.Expect(c.VolumeMounts[0].Name).To(Equal(CABundleVolumeName))
+	g.Expect(c.VolumeMounts[0].MountPath).To(Equal(CABundleMountDir))
+	g.Expect(c.VolumeMounts[0].SubPath).To(Equal(""), "subPath should be empty to enable rotation")
+	g.Expect(c.VolumeMounts[0].ReadOnly).To(BeTrue())
 }
 
 func TestWithAllowUnverifiedEmail(t *testing.T) {
