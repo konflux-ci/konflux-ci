ba85745aa79518d18b249637003738c2d411dee3
Francesco Ilario <filario@redhat.com>
2026-05-04 13:50:06 +0200

    remove long-lived ServiceAccount Token from Konflux UI
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    refactor to move scripts to dedicated files
    
    This change allow us to easily run shellcheck on them
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    use bash instead of sh
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    add liveness probe
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    add euo pipefail flags
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    wait for nginx to be ready in reload loop
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    implement hot reload at need
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

---
+++ operator/upstream-kustomizations/ui/core/proxy/proxy-nginx-generate-loop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+RETRY_INTERVAL=10
+TOKEN_FILEPATH=/var/run/secrets/konflux-ci.dev/serviceaccount/token
+AUTH_CONF_FILE=/mnt/nginx-generated-config/auth.conf.new
+AUTH_CONF_TMP_FILE=/mnt/nginx-generated-config/auth.conf.new.tmp
+AUTH_CONF_TEMPLATE_FILE=/mnt/nginx-templates/auth.conf
+
+produceToken() (
+  # Copy the auth.conf template and replace the bearer token
+  token=$(cat "${TOKEN_FILEPATH}")
+
+  # Produce a tmp file
+  sed "s/__BEARER_TOKEN__/${token}/" "${AUTH_CONF_TEMPLATE_FILE}" > "${AUTH_CONF_TMP_FILE}"
+  chmod 640 "${AUTH_CONF_TMP_FILE}"
+
+  # Rename (atomic) the file to avoid sync issues
+  mv "${AUTH_CONF_TMP_FILE}" "${AUTH_CONF_FILE}"
+)
+
+produceTokenWithRetry() (
+  produceToken || \
+    { sleep 3; produceToken; } || \
+    { sleep 5; produceToken; }
+)
+
+while produceTokenWithRetry; do sleep "${RETRY_INTERVAL}"; done
+
+echo "loop broke, crashing"
+exit 1
