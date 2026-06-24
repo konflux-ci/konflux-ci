25e0e3ddd0a64a858a0c4710c1592fe409558ac5
fullsend-fix <278716306+fullsend-ai-coder[bot]@users.noreply.github.com>
2026-06-24 05:54:08 +0000

    fix: update go-github-ratelimit v2 API usage
    Replace removed v1 API calls with v2 equivalents:
    - NewRateLimitWaiterClient → NewClient (returns *http.Client, no error)
    - WithSingleSleepLimit moved to github_secondary_ratelimit sub-package
    
    Addresses review feedback on #7503

--- test/go-tests/pkg/clients/github/client.go
+++ test/go-tests/pkg/clients/github/client.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gofri/go-github-ratelimit/v2/github_ratelimit"
+	"github.com/gofri/go-github-ratelimit/v2/github_ratelimit/github_secondary_ratelimit"
 	"github.com/google/go-github/v44/github"
 	"github.com/konflux-ci/konflux-ci/test/go-tests/pkg/utils"
 	"golang.org/x/oauth2"
@@ -23,10 +24,7 @@ func NewClient(token, organization string) (*Client, error) {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(context.Background(), ts)
 	// https://docs.github.com/en/rest/guides/best-practices-for-integrators?apiVersion=2022-11-28#dealing-with-secondary-rate-limits
-	rateLimiter, err := github_ratelimit.NewRateLimitWaiterClient(tc.Transport, github_ratelimit.WithSingleSleepLimit(time.Minute, nil))
-	if err != nil {
-		return &Client{}, err
-	}
+	rateLimiter := github_ratelimit.NewClient(tc.Transport, github_secondary_ratelimit.WithSingleSleepLimit(time.Minute, nil))
 
 	// Wrap with retry transport so all API calls automatically retry on 5xx
 	rateLimiter.Transport = utils.NewRetryTransport(rateLimiter.Transport)
